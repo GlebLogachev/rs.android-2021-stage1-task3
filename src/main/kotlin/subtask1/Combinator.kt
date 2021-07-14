eb4ce1c7f801b3031fd56f635e2c7b35af4fcb32
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-07-14 15:50:24 +0700

    feat : add solutions for tasks

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,26 @@
 package subtask1
 
 class Combinator {
-
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+        val nFactorial = factorial(n)
+        for (i in 1..n) {
+            val differenceNAndI = n - i
+            val factorialDifferenceNAndI = factorial(differenceNAndI)
+            val iFactorial = factorial(i)
+            val result = nFactorial / factorialDifferenceNAndI / iFactorial
+            
+            if (result >= m) return i
+        }
+        return null
+    }
+
+    private fun factorial(n: Int): Long {
+        var factorial = 1L
+        for (i in 1..n) {
+            factorial *= i
+        }
+        return factorial
     }
 }
